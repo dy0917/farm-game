ed89eaaf72946792f7c5552eaf598557cc95717c
Wenchong <huangkingsley@gmail.com>
2018-08-04 16:07:56 +1200

    anmination done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,94 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import TitleScreen from './ReactComponents/TitleScreen';
+import Ship from './GameComponents/Ship';
+import InputManager from './InputManager';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const width = 800;
+const height = window.innerHeight;
+const ratio = window.devicePixelRatio || 1;
+const GameState = {
+	StartScreen : 0,
+	Playing : 1,
+	GameOver : 2
+};
 
-export default App;
+
+class App extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			input: new InputManager(), 
+			screen: {
+				width: width,
+				height: height,
+				ratio: window.devicePixelRatio || 1        
+			},
+			gameState: GameState.StartScreen,
+			context: null
+		}
+		this.ship = null
+
+	}
+
+	componentDidMount() {
+		this.state.input.bindKeys();
+		const context = this.refs.canvas.getContext('2d');
+		this.setState({ context: context });
+
+		requestAnimationFrame(() => {this.update()});  
+	}
+
+	update() {
+		const keys = this.state.input.pressedKeys;
+		if (this.state.gameState === GameState.StartScreen && keys.enter) {
+			this.startGame();
+		}
+
+		if (this.state.gameState === GameState.Playing) {
+			this.clearBackground();
+			if (this.ship !== undefined && this.ship !== null) {
+				this.ship.update(keys);
+				this.ship.render(this.state);       
+			}
+		}
+		requestAnimationFrame(() => {this.update()});
+	}
+
+	clearBackground() {
+		const context = this.state.context;
+		context.save();
+		context.scale(this.state.screen.ratio, this.state.screen.ratio);
+		context.fillRect(0, 0, this.state.screen.width, this.state.screen.height);
+		context.globalAlpha = 1;
+	}
+
+	startGame() {
+		let ship = new Ship({
+			radius: 15,
+			speed: 2.5,
+			position: {
+				x: this.state.screen.width/2,
+				y: this.state.screen.height - 50
+			}});
+		this.ship = ship;
+
+		this.setState({
+			gameState: GameState.Playing
+		});
+	}
+
+	render() {
+		return (
+			<div>   
+			{ this.state.gameState === GameState.StartScreen && <TitleScreen /> }  
+			<canvas ref="canvas"
+			width={ this.state.screen.width * this.state.screen.ratio }
+			height={ this.state.screen.height * this.state.screen.ratio } />
+			</div>
+			)
+		}
+	}
+
+	export default App;
